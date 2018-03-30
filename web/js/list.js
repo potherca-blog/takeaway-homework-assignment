2ccff5bda970fd9f23dcc451a3340d671d9f62b0
Ben Peachey <potherca@gmail.com>
2018-03-30 22:06:00 +0000

    Changes JS Restaurant List logic
    - Removes all UI interaction
    - Makes the main logic return a Promise

--- web/js/list.js
+++ web/js/list.js
@@ -1,5 +1,5 @@
-(function (window, document, $, Mustache){
-    "use strict";
+(function (window, $, Mustache){
+    'use strict';
 
     window.THA = window.THA || {};
 
@@ -21,23 +21,23 @@
         }
     };
 
-    function populateList(p_aItems, p_oSortMap, p_sActiveItem) {
-        var oData, aSortItems, sRendered, sTemplate;
+    function populateList(p_sTemplate, p_aItems, p_oSortMap) {
+        var oData, aListItems, aSortItems, sRendered, $ListItems;
 
-        sTemplate = $('#restaurant-item-template').html();
+        aListItems = [];
 
-        Mustache.parse(sTemplate);
+        Mustache.parse(p_sTemplate);
 
         p_aItems.forEach(function (p_oItem) {
 
-            /*/ Populate sort-item values  /*/
             aSortItems = [];
+
             $.each(p_oSortMap, function (p_sKey, p_iValue){
                 var oItem;
 
                 oItem = p_iValue;
                 oItem.value = p_oItem.sortingValues[p_sKey];
-                oItem.style = p_sKey === p_sActiveItem ? 'info' : 'light';
+                oItem.key = p_sKey;
 
                 aSortItems.push(oItem);
             });
@@ -48,15 +48,20 @@
                 sortItems: aSortItems
             };
 
-            sRendered = Mustache.render(sTemplate, oData);
+            sRendered = Mustache.render(p_sTemplate, oData);
 
-            $('.restaurant-list').append(sRendered);
+            aListItems.push($(sRendered));
         });
+
+        /* Convert the array of jQuery objects to a single jQuery object */
+        $ListItems = aListItems.reduce($.merge);
+
+        return Promise.resolve($ListItems);
     }
 
     window.THA.list = {
-        populate: function (p_aItems, p_oSortMap, p_sActiveItem) {
-            populateList(p_aItems, p_oSortMap, p_sActiveItem);
+        populate: function (p_sTemplate, p_aItems, p_oSortMap) {
+            return populateList(p_sTemplate, p_aItems, p_oSortMap);
         },
     };
-}(window, document, jQuery, Mustache));
+}(window, jQuery, Mustache));
